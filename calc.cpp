eda8167436fc30aa5f54a7404a305ef415958bc9
FirefistisDead <yashco.ltd@gmail.com>
2024-03-15 21:52:37 +0530

    Added calculatot

---
+++ calc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class hello
+{
+    private:
+        /*variables and methods*/
+    public:
+        /*variables and methods*/
+};
+
+int main()
+{
+    cout<<"HELLO";
+    return 0;
+}
